8a697da66cc902323c9e92cebe922ca2449ace58
raghu.myworks@gmail.com <raghu.myworks@gmail.com>
2015-02-09 09:29:08 +0000

    init

---
+++ src/main/java/com/easylearnjava/example/config/AppConfig.java
@@ -0,0 +1,9 @@
+package com.easylearnjava.example.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.easylearnjava.example")
+class AppConfig {
+}
